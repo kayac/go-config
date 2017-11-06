b356fe37f4151fbf0be9fd66b306d994dc92d9ed
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-11-06 13:40:41 +0900

    first version

---
+++ config.go
@@ -0,0 +1,88 @@
+// Yaml Config Loader
+package config
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"text/template"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+func init() {
+	initEnvReplacer()
+}
+
+type customFunc func(data []byte) ([]byte, error)
+
+// Yaml files load from `configPaths`.
+// and assigns decoded values into the `conf` value.
+func Load(conf interface{}, configPaths ...string) error {
+	return loadWithFunc(conf, nil, configPaths)
+}
+
+// Load with Env
+// replace {{ env "ENV" }} to os.Getenv("ENV")
+// if you set default value then {{ env "ENV" "default" }}
+func LoadWithEnv(conf interface{}, configPaths ...string) error {
+	return loadWithFunc(conf, envReplacer, configPaths)
+}
+
+func loadWithFunc(conf interface{}, custom customFunc, configPaths []string) error {
+	for _, configPath := range configPaths {
+		err := loadConfig(configPath, conf, custom)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func loadConfig(configPath string, conf interface{}, custom customFunc) error {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return errors.Wrapf(err, "%s read failed", configPath)
+	}
+	if custom != nil {
+		data, err = custom(data)
+		if err != nil {
+			return errors.Wrapf(err, "%s yaml custom failed", configPath)
+		}
+	}
+	if err := yaml.Unmarshal(data, conf); err != nil {
+		return errors.Wrapf(err, "%s yaml parse failed", configPath)
+	}
+	return nil
+}
+
+var envRepTpl *template.Template
+
+func initEnvReplacer() {
+	envRepTpl = template.New("conf").Funcs(template.FuncMap{
+		"env": func(keys ...string) string {
+			v := ""
+			for _, k := range keys {
+				v = os.Getenv(k)
+				if v != "" {
+					return v
+				}
+				v = k
+			}
+			return v
+		},
+	})
+}
+
+func envReplacer(data []byte) ([]byte, error) {
+	t, err := envRepTpl.Parse(string(data))
+	if err != nil {
+		return nil, errors.Wrap(err, "config parse by template failed")
+	}
+	buf := &bytes.Buffer{}
+	if err = t.Execute(buf, nil); err != nil {
+		return nil, errors.Wrap(err, "template attach failed")
+	}
+	return buf.Bytes(), nil
+}
---
+++ config_test.go
@@ -0,0 +1,232 @@
+package config_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/kayac/go-config"
+)
+
+var dir string
+
+type DBConfig struct {
+	Master  string        `yaml:"master"`
+	Slave   string        `yaml:"slave"`
+	Timeout time.Duration `yml:"timeout"`
+}
+type Conf struct {
+	Domain  string        `yaml:"domain"`
+	IsDev   bool          `yaml:"is_dev"`
+	Timeout time.Duration `yml:"timeout"`
+	DB      DBConfig      `yaml:"db"`
+}
+
+func TestMain(m *testing.M) {
+	runner := func() int {
+		var err error
+		dir, err = ioutil.TempDir("", "go-config")
+		if err != nil {
+			panic(err)
+		}
+		defer os.RemoveAll(dir)
+		return m.Run()
+	}
+	os.Exit(runner())
+}
+
+func ExampleLoad() {
+	type DBConfig struct {
+		Master  string        `yaml:"master"`
+		Slave   string        `yaml:"slave"`
+		Timeout time.Duration `yml:"timeout"`
+	}
+	type Conf struct {
+		Domain  string        `yaml:"domain"`
+		IsDev   bool          `yaml:"is_dev"`
+		Timeout time.Duration `yml:"timeout"`
+		DB      DBConfig      `yaml:"db"`
+	}
+
+	baseConfig := `
+# config.yml
+domain: example.com
+db:
+  master:  rw@/example
+  slave:   ro@/example
+  timeout: 0.5s
+`
+	localConfig := `
+# config_local.yml
+domain: dev.example.com
+is_dev: true
+`
+	conf := &Conf{}
+	baseConfigYaml, _ := genConfigYaml("config.yml", baseConfig)         // /path/to/config.yml
+	localConfigYaml, _ := genConfigYaml("config_local.yml", localConfig) // /path/to/config_local.yml
+
+	err := config.Load(conf, baseConfigYaml, localConfigYaml)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", conf)
+	// Output:
+	// &{Domain:dev.example.com IsDev:true Timeout:0s DB:{Master:rw@/example Slave:ro@/example Timeout:500ms}}
+}
+
+func ExampleLoadWithEnv() {
+	type DBConfig struct {
+		Master  string        `yaml:"master"`
+		Slave   string        `yaml:"slave"`
+		Timeout time.Duration `yml:"timeout"`
+	}
+	type Conf struct {
+		Domain  string        `yaml:"domain"`
+		IsDev   bool          `yaml:"is_dev"`
+		Timeout time.Duration `yml:"timeout"`
+		DB      DBConfig      `yaml:"db"`
+	}
+
+	baseConfig := `
+# config.yml
+domain: {{ env "DOMAIN"}}
+db:
+  master:  rw@/example
+  slave:   ro@/example
+  timeout: 0.5s
+`
+	localConfig := `
+# config_local.yml
+is_dev: true
+db:
+  master:  {{ env "RW_USER" "rw" }}@/{{ env "DB_NAME" "example_dev" }}
+  slave:   {{ env "RO_USER" "ro" }}@/{{ env "DB_NAME" "example_dev" }}
+`
+	os.Setenv("DOMAIN", "dev.example.com")
+	os.Setenv("DB_NAME", "example_local")
+
+	conf := &Conf{}
+	baseConfigYaml, _ := genConfigYaml("config.yml", baseConfig)         // /path/to/config.yml
+	localConfigYaml, _ := genConfigYaml("config_local.yml", localConfig) // /path/to/config_local.yml
+
+	err := config.LoadWithEnv(conf, baseConfigYaml, localConfigYaml)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", conf)
+	// Output:
+	// &{Domain:dev.example.com IsDev:true Timeout:0s DB:{Master:rw@/example_local Slave:ro@/example_local Timeout:500ms}}
+}
+
+func TestLoad(t *testing.T) {
+	a, err := genConfigYaml("a.yml", `## a.yml
+domain: example.com
+db:
+  master:  rw@/example
+  slave:   ro@/example
+  timeout: 0.5s
+`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	b, err := genConfigYaml("b.yml", `## b.yml
+is_dev: true
+`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	c, err := genConfigYaml("c.yml", `## c.yml
+db:
+  master:  rw@/example2
+  slave:   ro@/example2
+  timeout: 200ms
+`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	er, err := genConfigYaml("err.yml", `## err.yml
+db:
+  master  rw@/example2
+`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	e := &Conf{
+		Domain:  "example.com",
+		IsDev:   false,
+		Timeout: time.Duration(0),
+		DB: DBConfig{
+			Master:  "rw@/example",
+			Slave:   "ro@/example",
+			Timeout: time.Duration(500) * time.Millisecond,
+		},
+	}
+	t1 := &Conf{}
+	err = config.Load(t1, a)
+	if err != nil {
+		t.Errorf("a.yml load error %s", err)
+	}
+	if !reflect.DeepEqual(t1, e) {
+		t.Errorf("a.yml not match. got: %#v, expect: %#v", t1, e)
+	}
+
+	t2 := &Conf{}
+	e.IsDev = true
+	err = config.Load(t2, a, b)
+	if err != nil {
+		t.Errorf("a.yml, b.yml load error %s", err)
+	}
+	if !reflect.DeepEqual(t2, e) {
+		t.Errorf("a.yml, b.yml not match. got: %#v, expect: %#v", t2, e)
+	}
+
+	t3 := &Conf{}
+	e.DB.Master = "rw@/example2"
+	e.DB.Slave = "ro@/example2"
+	e.DB.Timeout = time.Duration(200) * time.Millisecond
+	err = config.Load(t3, a, b, c)
+	if err != nil {
+		t.Errorf("a.yml, b.yml, c.yml load error %s", err)
+	}
+	if !reflect.DeepEqual(t3, e) {
+		t.Errorf("a.yml, b.yml, c.yml not match. got: %#v, expect: %#v", t3, e)
+	}
+
+	t4 := &Conf{}
+	err = config.Load(t4, filepath.Join(dir, "nothing.yml"))
+	if err == nil {
+		t.Errorf("nothing.yml is not found.")
+	}
+	t.Log(err)
+
+	t5 := &Conf{}
+	err = config.Load(t5, er)
+	if err == nil {
+		t.Errorf("err.yml is format err.")
+	}
+	t.Log(err)
+
+}
+
+func genConfigYaml(name string, config string) (string, error) {
+	path := filepath.Join(dir, name)
+	io, err := os.Create(path)
+	if err != nil {
+		return "", err
+	}
+	if _, err := io.WriteString(config); err != nil {
+		return "", err
+	}
+	return path, nil
+}
